474d5dfd9bdf850a4369597e217e5f454092ef1e
Nodirbek Ergashev <mr.nodirbek77@gmail.com>
2022-07-22 12:26:02 -0500

    feat: Task List

---
+++ task.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+    updateHistory();
+}
+
+function addTask() {
+    let task = document.getElementById("task-name").value;
+
+    if (task.length === 0) {
+        alert("Please enter text");
+        return;
+    }
+
+    let history = localStorage.getItem("list");
+    if (history == null) {
+        history = "";
+    }
+    history = task + "\r\n" + history;
+    localStorage.setItem("list", history);
+
+    // update textarea value
+    updateHistory();
+
+    // clear input
+    document.getElementById("task-name").value = "";
+}
+
+function clearTask() {
+    let historyList = document.getElementById("history-list");
+    historyList.value = "";
+    // TODO: remove from localStorage
+    localStorage.removeItem("list");
+}
+
+function updateHistory() {
+    let history = document.getElementById("history-list");
+    history.value = localStorage.getItem("list");
+}
